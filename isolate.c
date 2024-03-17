addf7ee3939a6e202bc315b1fc60bf946f5f061a
Sahil Pahuja <sahilpahuja963@gmail.com>
2024-03-17 20:58:05 +0530

    Always set the file size limit.

--- isolate.c
+++ isolate.c
@@ -804,8 +804,7 @@ setup_rlimits(void)
   if (memory_limit)
     RLIM(AS, (rlim_t)memory_limit * 1024);
 
-  if (fsize_limit)
-    RLIM(FSIZE, (rlim_t)fsize_limit * 1024);
+  RLIM(FSIZE, (rlim_t)fsize_limit * 1024);
 
   if (open_file_limit)
     RLIM(NOFILE, (rlim_t)open_file_limit);
